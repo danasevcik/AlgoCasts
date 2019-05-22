0034993fef026247441000b7074bb04cbc05c8cb
github username <danasevcik@hotmail.com>
2019-05-22 16:07:03 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,40 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// MY SOLUTION BEFORE VIDEO
+// CREATE A HASH
+// ITERATE, CHECK IF THE CHAR IS A KEY
+// IF YES
+  // INCREASE VALUE BY 1
+// IF NO
+  // ADD KEY WITH VALUE OF 0
+// SORT BY VALUE
+// RETURN LARGEST KEY
+
+function maxChar(str) {
+  let charHash = {}
+  let arr = str.split("")
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    let char = arr[i]
+    if (Object.keys(charHash).includes(char)) {
+      charHash[char] ++
+    } else {
+      charHash[char] = 1
+    }
+  }
+
+  let keys = Object.keys(charHash)
+  keys.sort(function(a, b) {
+    return charHash[a] - charHash[b]
+  }).reverse()
+
+  console.log(keys);
+  console.log(keys[0]);
+  
+  return keys[0]
+}
+
+maxChar('hello')
 
 module.exports = maxChar;
