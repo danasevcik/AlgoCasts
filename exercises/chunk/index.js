83dddf6141bb55beb5e8eb2b41c07b6902c9be7c
github username <danasevcik@hotmail.com>
2019-05-23 10:37:08 -0400

    array chunks

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+// MY SOLUTION BEFORE VIDEO
+// create new array to push into
+// get the number of chunks that will be in the final array
+// iterate over the number of chunks
+// for each iteration, push a sliced chunk of the coped array into the new array
+// return array at the end
+
+function chunk(array, size) {
+  let newArray = []
+  let copiedArray = [...array]
+  let numOfChunks = Math.ceil(copiedArray.length / size)
+  for (let i = array[0]; i < numOfChunks; i ++) {
+    newArray.push(copiedArray.splice(0, size))
+  }
+  console.log(newArray);
+  return newArray
+}
+
+chunk([1,2,3,4,5,6,7,8], 2)
 
 module.exports = chunk;
