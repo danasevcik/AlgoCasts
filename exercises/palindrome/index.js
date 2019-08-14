867c8c82677ce412d042f6138df6579f8fd236f3
Dana Sevcik <danasevcik@hotmail.com>
2019-08-14 14:36:28 -0400

    palindrome 1 loop

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,50 +7,91 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+function palindrome(str) {
+  // split string in half
+  // iterate both ways and see if they are equal
+  let midPoint = str.length / 2
+  for (let i = 1; i < (str.length / 2); i++ ) {
+    if (midPoint % 2 === 0) {
+      if (str[midPoint] !== str[midPoint - i]) {
+        return false
+      }
+    } else if (midPoint % 2 !== 0) {
+      if (str[Math.ceil(midPoint) - (i-1)] !== str[Math.ceil(midPoint)]) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+
+// console.log(palindrome(' aba'));
+console.log(palindrome('aba'));
+
+// midpoint is 1.5 will now be 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // MY SOLUTION BEFORE VIDEO
 // LOOP THROUGH FORWARDS
 // LOOP THROUGH BACKWARDS
 // IF ITS NOT THE SAME, RETURN FALSE
 // AT THE VERY END RETURN TRUE
 
-function palindrome(str) {
-  for (let i = 0; i < str.length - 1; i++) {
-    for (let j = str.length - 1; j > 0; j--) {
-      if (str[i] !== str[j]) {
-        // console.log('false');
-        return false
-      }
-    }
-    return true
-  }
-}
+// function palindrome(str) {
+//   for (let i = 0; i < str.length - 1; i++) {
+//     for (let j = str.length - 1; j > 0; j--) {
+//       if (str[i] !== str[j]) {
+//         // console.log('false');
+//         return false
+//       }
+//     }
+//     return true
+//   }
+// }
 
 // palindrome('hello')
-console.log(palindrome('hannah'))
-console.log(palindrome('haaannnnnnnnnnh'))
+// console.log(palindrome('hannah'))
+// console.log(palindrome('haaannnnnnnnnnh'))
 
 // Algo study group solution 1
-function palindrome(str) {
-
-    if (str.length < 2) {
-        return true;
-    } else {
-        const firstChar = str[0];
-        const lastChar = str[str.length - 1];
-
-        return firstChar === lastChar ? palindrome(str.slice(1, -1)) : false;
-    }
-
-}
+// function palindrome(str) {
+//
+//     if (str.length < 2) {
+//         return true;
+//     } else {
+//         const firstChar = str[0];
+//         const lastChar = str[str.length - 1];
+//
+//         return firstChar === lastChar ? palindrome(str.slice(1, -1)) : false;
+//     }
+//
+// }
 
 // Algo study group solution 2
-function palindrome(str) {
-  for (let i = 0, j = str.length-1; i < j; i++, j--) {
-    if (str[i] !== str[j]) {
-      return false
-    }
-  }
-  return true
-}
+// function palindrome(str) {
+//   for (let i = 0, j = str.length-1; i < j; i++, j--) {
+//     if (str[i] !== str[j]) {
+//       return false
+//     }
+//   }
+//   return true
+// }
 
 module.exports = palindrome;
